1e18cf7227cd98043d2fdcd0f0b5a59406a35117
v_llongfang <v_llongfang@tencent.com>
2024-06-17 17:14:51 +0800

    init

---
+++ src/mock/menu.ts
@@ -0,0 +1,168 @@
+[
+  {
+    "id": 1,
+    "name": "权限管理",
+    "level": 0,
+    "path": "1",
+    "href": "",
+    "children": [
+      {
+        "id": 11,
+        "name": "角色列表",
+        "level": 1,
+        "path": "1-11",
+        "href": "/api/v1/list-role",
+        "children": [
+          {
+            "id": 111,
+            "name": "角色添加",
+            "level": 2,
+            "path": "1-11-111",
+            "href": "/api/v1/add-role"
+          },
+          {
+            "id": 112,
+            "name": "角色修改",
+            "level": 2,
+            "path": "1-11-112",
+            "href": "/api/v1/update-role"
+          },
+          {
+            "id": 113,
+            "name": "角色删除",
+            "level": 2,
+            "path": "1-11-113",
+            "href": "/api/v1/remove-role"
+          },
+          {
+            "id": 114,
+            "name": "角色查询",
+            "level": 2,
+            "path": "1-11-114",
+            "href": "/api/v1/query-role"
+          }
+        ]
+      },
+      {
+        "id": 12,
+        "name": "用户列表",
+        "level": 1,
+        "path": "1-12",
+        "href": "/api/v1/list-user",
+        "children": [
+          {
+            "id": 121,
+            "name": "用户添加",
+            "level": 2,
+            "path": "1-12-121",
+            "href": "/api/v1/add-user"
+          },
+          {
+            "id": 122,
+            "name": "用户修改",
+            "level": 2,
+            "path": "1-12-122",
+            "href": "/api/v1/update-user"
+          },
+          {
+            "id": 123,
+            "name": "用户删除",
+            "level": 2,
+            "path": "1-12-123",
+            "href": "/api/v1/remove-user"
+          },
+          {
+            "id": 124,
+            "name": "用户查询",
+            "level": 2,
+            "path": "1-12-124",
+            "href": "/api/v1/query-user"
+          }
+        ]
+      },
+      {
+        "id": 13,
+        "name": "权限列表",
+        "level": 1,
+        "path": "1-13",
+        "href": "/api/v1/list-auth",
+        "children": [
+          {
+            "id": 131,
+            "name": "权限添加",
+            "level": 2,
+            "path": "1-13-131",
+            "href": "/api/v1/add-auth"
+          },
+          {
+            "id": 132,
+            "name": "权限修改",
+            "level": 2,
+            "path": "1-13-132",
+            "href": "/api/v1/update-auth"
+          },
+          {
+            "id": 133,
+            "name": "权限删除",
+            "level": 2,
+            "path": "1-12-133",
+            "href": "/api/v1/remove-auth"
+          },
+          {
+            "id": 134,
+            "name": "权限查询",
+            "level": 2,
+            "path": "1-13-134",
+            "href": "/v1/query-auth"
+          }
+        ]
+      }
+    ]
+  },
+  {
+    "id": 2,
+    "name": "系统管理",
+    "level": 0,
+    "path": "2",
+    "href": "",
+    "children": [
+      {
+        "id": 21,
+        "name": "日志管理",
+        "level": 1,
+        "path": "2-21",
+        "href": "/api/v1/list-system-log",
+        "children": [
+          {
+            "id": 211,
+            "name": "日志添加",
+            "level": 2,
+            "path": "2-21-211",
+            "href": "/api/v1/add-system-log"
+          },
+          {
+            "id": 212,
+            "name": "日志修改",
+            "level": 2,
+            "path": "2-21-212",
+            "href": "/api/v1/update-system-log"
+          },
+          {
+            "id": 213,
+            "name": "日志删除",
+            "level": 2,
+            "path": "2-21-213",
+            "href": "/api/v1/remove-system-log"
+          },
+          {
+            "id": 214,
+            "name": "日志查询",
+            "level": 2,
+            "path": "2-21-214",
+            "href": "/api/v1/query-system-log"
+          }
+        ]
+      }
+    ]
+  }
+]
